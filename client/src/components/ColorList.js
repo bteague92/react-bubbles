f81e933f0e1bc79f42b68473446c4eb0f2d9a553
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-11-15 14:49:14 -0500

    fixed edit and delete functions

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "./../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -17,30 +17,30 @@ const ColorList = ({ colors, updateColors }) => {
   };
 
   const saveEdit = e => {
-    e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-    axios
-      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
       .then(res => {
         console.log(res);
-        setColorToEdit(res.data);
+        colors.map(i => {
+          if (i.id === colorToEdit.id) {
+            setColorToEdit({
+              [e.target.name]: e.target.value
+            })
+          }
+        })
       })
-      .catch(err => console.log("something went wrong with edit"))
+      .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
-
-    axios
-      .delete(`http://localhost:5000/api/colors/${color.id}`)
+    axiosWithAuth()
+      .delete(`/api/colors/${color.id}`)
       .then(res => {
         console.log(res);
-        updateColors(res.data);
+        const newArray = colors.filter(i => i.id !== color.id)
+        updateColors(newArray)
       })
-      .catch(err => console.log("something went wrong with delete"))
-
+      .catch(err => console.log(err))
   };
 
   return (
